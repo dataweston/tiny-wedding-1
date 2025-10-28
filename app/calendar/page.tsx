3f02d2dea468ec08fd4a48f369f004526533c8a7
terroroir <81487235+terroroir@users.noreply.github.com>
2025-10-28 15:09:36 -0500

    Ensure 'use client' is the first line in calendar page to satisfy directive order

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = 'force-dynamic'
+ 'use client'
 
-'use client'
+export const dynamic = 'force-dynamic'
 
 import { useState, Suspense } from 'react'
 import { useSearchParams, useRouter } from 'next/navigation'
