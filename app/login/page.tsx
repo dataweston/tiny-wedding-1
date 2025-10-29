8e86007c0cda5f6219e131c05646f8e036d65d12
terroroir <81487235+terroroir@users.noreply.github.com>
2025-10-28 23:05:01 -0500

    s

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -95,7 +95,7 @@ export default function LoginPage() {
             </Button>
 
             <div className="text-center text-sm text-gray-600">
-              Don't have an account?{' '}
+              Don&apos;t have an account?{' '}
               <Link href="/signup" className="text-rose-600 hover:underline">
                 Sign up
               </Link>
