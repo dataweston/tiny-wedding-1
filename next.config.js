09bf479701b758ef09ebb0c2f608e2de09c2af2f
terroroir <81487235+terroroir@users.noreply.github.com>
2025-10-29 10:33:30 -0500

    s

--- next.config.js
+++ next.config.js
@@ -11,6 +11,23 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOW-FROM https://localeffortfood.com',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://localeffortfood.com https://*.localeffortfood.com http://localhost:*",
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
