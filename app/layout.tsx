c7e1bc163646a0f15480313f78d6a4d4631c9cc2
terroroir <81487235+terroroir@users.noreply.github.com>
2025-10-30 16:29:36 -0500

    s

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,18 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link href="https://fonts.googleapis.com/css2?family=Funnel+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-P0Q3W8KEKY" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-P0Q3W8KEKY');
+            `,
+          }}
+        />
       </head>
       <body className={`${tangerine.variable} ${caveat.variable} ${patrickHandSc.variable}`}>
         <Navigation />
